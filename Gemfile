d3f53651b5db0f0791b88ff0a6159147d702de9d
Nelson Agamata <nelson_s_agamata@yahoo.com>
2014-09-30 22:31:10 +0800

    Added jquery masonry

--- Gemfile
+++ Gemfile
@@ -7,10 +7,12 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'jquery-turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', '3.2.0.2'
 gem 'devise', '~> 3.1.0.rc2'
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'masonry-rails', '~> 0.2.0'
 
 group :development, :test do
 	gem 'sqlite3', '1.3.8'
@@ -24,4 +26,5 @@ end
 group :production do
 	gem 'pg', '0.15.1'
 	gem 'rails_12factor', '0.0.2'
+	gem 'aws-sdk', '~> 1.20.0'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Pinteresting::Application.configure do
 
   # Define default url options in production environment files
   config.action_mailer.default_url_options = { :host => 'http://calm-shelf-6157.herokuapp.com' }
+
+  # Sets paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
